b52ed103ed44ff237d9b140f4bee9b76b152d613
Alexander-W-Mercer <alwmerce@ucsc.edu>
2025-11-13 00:27:16 +0000

    Fixed clear button with new points system

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,34 @@ document.body.append(clearButton);
 const ctx = canvas.getContext("2d");
 let isDrawing = false;
 
+const drawnPoints: number[][] = [];
+
 // Force drawing buffer size to match display === The fact that I have to do this is really annoying.
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
+
+function drawingChanged() {
+  if (ctx && drawnPoints) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath(); // Start a new path
+
+    // Move to the first point without drawing
+    if (drawnPoints.length > 0) {
+      ctx.moveTo(drawnPoints[0]![0]!, drawnPoints[0]![1]!);
+    }
+
+    // Iterate through the remaining points and draw lines to them
+    for (let i = 1; i < drawnPoints.length; i++) {
+      ctx.lineTo(drawnPoints[i]![0]!, drawnPoints[i]![1]!);
+    }
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+
+    ctx.stroke(); // Render the line
+  }
+}
 
 if (ctx) {
   canvas.addEventListener("mousedown", (e) => {
@@ -30,8 +55,7 @@ if (ctx) {
     const rect = canvas.getBoundingClientRect();
     const x = e.clientX - rect.left - 10; // magic number 10 is just to get it to the mouse pointer tip
     const y = e.clientY - rect.top - 10;
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    drawnPoints.push([x, y]);
   });
 
   canvas.addEventListener("mousemove", (e) => {
@@ -39,14 +63,17 @@ if (ctx) {
       const rect = canvas.getBoundingClientRect();
       const x = e.clientX - rect.left - 10; // magic number 10 is just to get it to the mouse pointer tip
       const y = e.clientY - rect.top - 10;
+      drawnPoints.push([x, y]);
 
-      ctx.lineTo(x, y);
-      ctx.stroke();
+      console.log(drawnPoints);
+
+      drawingChanged();
     }
   });
 
   canvas.addEventListener("mouseup", () => {
     isDrawing = false;
+    drawnPoints.push([-1, -1]);
   });
 
   canvas.addEventListener("mouseleave", () => {
@@ -55,6 +82,8 @@ if (ctx) {
 
   clearButton.addEventListener("click", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawnPoints.length = 0;
+    drawnPoints.push([-1, -1]);
   });
 
   ctx.fillStyle = "green";
