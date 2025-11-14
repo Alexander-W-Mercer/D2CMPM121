4ca7ecd7f635578478f11f79dcfd6894d1e6335c
Alexander-W-Mercer <alwmerce@ucsc.edu>
2025-11-14 00:35:29 +0000

    removed useless code from undo function, Added rudimentary brush size slider

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,48 @@ const redoButton = document.createElement("button");
 redoButton.id = "redo";
 redoButton.textContent = "Redo";
 
+//--------------------
+// Create slider container
+const container = document.createElement("div");
+container.style.margin = "20px";
+
+// Create label
+const label = document.createElement("label");
+label.textContent = "Brush Size: ";
+label.htmlFor = "size-slider";
+
+// Create span to show value
+const valueDisplay = document.createElement("span");
+valueDisplay.id = "value-display";
+valueDisplay.textContent = "5";
+
+// Create the slider input
+const slider = document.createElement("input");
+slider.type = "range";
+slider.id = "size-slider";
+slider.min = "1";
+slider.max = "50";
+slider.value = "5";
+slider.step = "1";
+slider.style.width = "200px";
+
+// Update display when slider changes
+slider.addEventListener("input", () => {
+  valueDisplay.textContent = slider.value;
+});
+
+// Append everything to the container
+container.appendChild(label);
+container.appendChild(valueDisplay);
+container.appendChild(document.createTextNode("px"));
+container.appendChild(document.createElement("br"));
+container.appendChild(slider);
+
+//--------------------
+
 document.body.append(h1);
 document.body.append(canvas);
+document.body.appendChild(container);
 document.body.append(clearButton);
 document.body.append(undoButton);
 document.body.append(redoButton);
@@ -33,6 +73,10 @@ let isDrawing = false;
 const drawnPoints: number[][][] = [];
 let segmentsDrawn = 0;
 const undoHolder: number[][][] = [];
+
+//interface drawnLines {
+//  points: number[][];
+//}
 
 // Force drawing buffer size to match display === The fact that I have to do this is really annoying.
 canvas.width = canvas.clientWidth;
@@ -62,7 +106,7 @@ function drawingChanged() {
       }
 
       ctx.strokeStyle = "black";
-      ctx.lineWidth = 2;
+      ctx.lineWidth = +slider.value;
 
       ctx.stroke(); // Render the line
     }
@@ -115,17 +159,9 @@ if (ctx) {
 
   undoButton.addEventListener("click", () => {
     if (drawnPoints[0]) {
-      console.log(undoHolder);
       undoHolder.push(drawnPoints.pop()!);
       segmentsDrawn--;
-      console.log(undoHolder);
-      if (drawnPoints.length > 0) {
-        drawingChanged();
-      } else {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawnPoints.length = 0;
-        segmentsDrawn = 0;
-      }
+      drawingChanged();
     } else {
       console.log("Nothing left to undo");
     }
@@ -133,10 +169,8 @@ if (ctx) {
 
   redoButton.addEventListener("click", () => {
     if (undoHolder[0]) {
-      console.log(undoHolder);
       drawnPoints.push(undoHolder.pop()!);
       segmentsDrawn++;
-      console.log(undoHolder);
       drawingChanged();
     } else {
       console.log("Nothing left to redo");
